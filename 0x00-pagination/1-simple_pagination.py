a294bae75137345657ab0e599ca3bcce8f7ba0b2
noblet <appiahpatricknoblet@gmail.com>
2024-08-19 23:49:22 +0100

    Added a module provides a simple server class to paginate a dataset of popular baby names

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -25,6 +25,7 @@ from typing import List, Tuple
 
 
 class Server:
+    """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -33,6 +34,23 @@ class Server:
         set to None.
         """
         self.__dataset = None
+
+    @staticmethod
+    def index_range(page: int, page_size: int) -> Tuple[int, int]:
+        """
+        Computes the start and end indices for a given page and page size.
+
+        Args:
+            page (int): The page number (1-indexed).
+            page_size (int): The number of items per page.
+
+        Returns:
+            Tuple[int, int]: A tuple containing the start and end indices
+            for the dataset.
+        """
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        return start_index, end_index
 
     def dataset(self) -> List[List]:
         """
@@ -52,23 +70,6 @@ class Server:
             self.__dataset = dataset[1:]
 
         return self.__dataset
-
-    @staticmethod
-    def index_range(page: int, page_size: int) -> Tuple[int, int]:
-        """
-        Computes the start and end indices for a given page and page size.
-
-        Args:
-            page (int): The page number (1-indexed).
-            page_size (int): The number of items per page.
-
-        Returns:
-            Tuple[int, int]: A tuple containing the start and end indices
-            for the dataset.
-        """
-        start_index = (page - 1) * page_size
-        end_index = start_index + page_size
-        return start_index, end_index
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
@@ -94,6 +95,7 @@ class Server:
 
         start_index, end_index = self.index_range(page, page_size)
         dataset = self.dataset()
+        
         try:
             return dataset[start_index:end_index]
         except IndexError:
