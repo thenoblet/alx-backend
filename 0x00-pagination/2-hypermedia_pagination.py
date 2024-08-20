b00dd1ce02cb89a93a425d92555202b8b1fe0ce4
noblet <appiahpatricknoblet@gmail.com>
2024-08-20 02:10:16 +0100

    Added a method get_hyper(page: int, page_size: int) -> dict: Retrieves a specific page       of data along with hypermedia metadata.

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -134,7 +134,7 @@ class Server:
                 "page": page,
                 "data": data,
                 "next_page": page + 1 if page < total_pages else None,
-                "prev_page": page - 1 if page > 0 else None,
+                "prev_page": page - 1 if page > 1 else None,
                 "total_pages": total_pages
         }
 
