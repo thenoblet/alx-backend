72d86bebcd565b42013d76188d77cb055d85a715
noblet <appiahpatricknoblet@gmail.com>
2024-09-26 23:27:55 +0100

    Added a basic script that connects to redis

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+import { createClient } from "redis";
+
+/**
+ * Initializes a Redis client and sets up event listeners to handle connection
+ * status and errors.
+ * 
+ * The Redis client connects to the Redis server and logs a message when connected successfully.
+ * If there is an error during connection, it logs an error message.
+ * 
+ * @constant {RedisClient}
+ */
+
+const client = createClient()
+	
+client
+	.on('error', (error) => console.error('Redis client not connected to the server:', error))
+	.on('connect', () => console.log('Redis client connected to the server'))
